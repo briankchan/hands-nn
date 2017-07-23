37ab7fa152ebb5da6d999443ae1dece13acb94d0
iluvredwall <bobomb.asplode@gmail.com>
2017-07-23 01:08:09 -0700

    print training progress more often; stop forcing gpu for everything; make training indices more flexible

--- tfmodel.py
+++ tfmodel.py
@@ -26,22 +26,22 @@ class TFModel(Model, metaclass=ABCMeta):
         self.config = tf.ConfigProto(allow_soft_placement=True)  # log_device_placement=True
         self.graph = tf.Graph()
         with self.graph.as_default():
-            with self.graph.device("/gpu:0"):
-                self.step = tf.Variable(0, trainable=False, name="step")
-                self.images, self.target_labels = self._build_inputs()
+            # with self.graph.device("/gpu:0"):
+            self.step = tf.Variable(0, trainable=False, name="step")
+            self.images, self.target_labels = self._build_inputs()
 
-                self.logits = self._build_network(self.images)
-                self.pred_labels = tf.greater(self.logits, 0)
+            self.logits = self._build_network(self.images)
+            self.pred_labels = tf.greater(self.logits, 0)
 
-                self.loss = self._build_loss(self.logits, self.target_labels, self.pos_weight)
-                self.optimizer = self._build_optimizer(self.loss, self.rate, self.epsilon, self.step)
+            self.loss = self._build_loss(self.logits, self.target_labels, self.pos_weight)
+            self.optimizer = self._build_optimizer(self.loss, self.rate, self.epsilon, self.step)
 
-                self.confusion_matrix = self._build_evaluator(self.target_labels, self.pred_labels)
+            self.confusion_matrix = self._build_evaluator(self.target_labels, self.pred_labels)
 
-                self.reset()
+            self.reset()
 
-                self.summary = tf.summary.merge_all()
-                self.saver = tf.train.Saver()
+            self.summary = tf.summary.merge_all()
+            self.saver = tf.train.Saver()
 
     def _build_inputs(self):
         with tf.name_scope("input"):
@@ -85,9 +85,9 @@ class TFModel(Model, metaclass=ABCMeta):
 
     def _reset_model(self):
         with self.graph.as_default():
-            with self.graph.device("/gpu:0"):
-                self.session = tf.Session(graph=self.graph, config=self.config)
-                self._initialize_model()
+            # with self.graph.device("/gpu:0"):
+            self.session = tf.Session(graph=self.graph, config=self.config)
+            self._initialize_model()
 
     def _initialize_model(self):
         self.session.run(tf.global_variables_initializer())
@@ -96,9 +96,11 @@ class TFModel(Model, metaclass=ABCMeta):
         if indices is None:
             indices = list(range(len(images)))
         else:
-            indices = list(indices)
+            indices = np.r_[tuple(indices)]
+
         if epochs == None:
             epochs = self.epochs
+
         if batch_size == None:
             batch_size = self.batch_size
 
@@ -111,16 +113,15 @@ class TFModel(Model, metaclass=ABCMeta):
             start = time.time()
 
             for i, frames in enumerate(batches, 1):
-                # _, losses = self.session.run([train_op, tf.get_collection('losses')],
                 summary, _ = self.session.run([self.summary, self.optimizer],
                                               {self.images: images[frames], self.target_labels: labels[frames]})
                 writer.add_summary(summary, tf.train.global_step(self.session, self.step))
-                if i % 10 == 0 or i == num_batches:
-                    print("Epoch {}: batch {} of {} | {:.3f}s".format(
-                        epoch,
-                        i,
-                        num_batches,
-                        time.time() - start), end="\r")
+                # if i % 10 == 0 or i == num_batches:
+                print("Epoch {}: batch {} of {} | {:.3f}s".format(
+                    epoch,
+                    i,
+                    num_batches,
+                    time.time() - start), end="\r")
             print()
 
         writer.close()
@@ -181,6 +182,4 @@ class TFModel(Model, metaclass=ABCMeta):
         self.saver.save(self.session, path+"/model.ckpt")
 
     def _load_model(self, path):
-        # with self.graph.as_default():
-        #     with self.graph.device("/gpu:0"):
         self.saver.restore(self.session, path+"/model.ckpt")
